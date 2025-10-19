f0073ac0b755aebadac90349a6d982dc8ddf8ff5
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-10-19 16:58:22 -0600

    test: clietn and item integration tests

---
+++ src/test/java/com/pikolinc/meliecommerce/integration/OrderControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.pikolinc.meliecommerce.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class OrderControllerIntegrationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
