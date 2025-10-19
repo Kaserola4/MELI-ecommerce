db6a3b4c9aefb5834d4c638dc290526742dea029
Dani <79950554+Kaserola4@users.noreply.github.com>
2025-10-19 12:55:23 -0600

    test: integration tests of ClientOrderController

---
+++ src/test/java/com/pikolinc/meliecommerce/integration/ClientControllerIntegrationTest.java
@@ -0,0 +1,13 @@
+package com.pikolinc.meliecommerce.integration;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+
+@SpringBootTest
+class ClientControllerIntegrationTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
